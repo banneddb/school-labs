0e364d9d7ba327a6821fdea65b1c90609723684b
Behruz Omon <behruzomon6@gmail.com>
2024-10-02 15:59:36 -0400

    commit

--- Lab 4 (and 5) - For Loops/Untitled-1.py
+++ Lab 4 (and 5) - For Loops/Untitled-1.py
@@ -1,9 +1,6 @@
-num = int(input("What is the integer for the multiplication table?"))
-
-def multiplication_table(num):
-    for row in range(1, num+1):
-        for collum in range(1, num+1):
-            print(row*collum, end="  ")
-        print()
-
-multiplication_table(num)
+total_bottles = (int(input("How many rounds of beers?")))
+def bottlesofbeer(n):
+    for bottles_left in range(total_bottles, 0, -1):
+        print(bottles_left,"bottles of beer on the wall", bottles_left, "bottles of beer" )
+        print("Take one down, pass it around", bottles_left-1, "bottles of beer on the wall")
+bottlesofbeer(total_bottles)
