f4aa90a11dd7ac1ae4d8949a595fd9fa3a7c670f
Behruz Omon <behruzomon6@gmail.com>
2024-10-02 15:55:39 -0400

    Delete Lab 4 (and 5) - For Loops/Untitled-1.py

--- Lab 4 (and 5) - For Loops/Untitled-1.py
+++
@@ -1,9 +0,0 @@
-num = int(input("What is the integer for the multiplication table?"))
-
-def multiplication_table(num):
-    for row in range(1, num+1):
-        for collum in range(1, num+1):
-            print(row*collum, end="  ")
-        print()
-
-multiplication_table(num)
